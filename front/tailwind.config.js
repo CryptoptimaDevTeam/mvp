03a1cd894911bacc77608409904c6d93c3510cc4
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-05-11 01:46:58 +0900

    Iss20 (#25)
    * setting front-end development evironment
    
    * feat - #20: Header Component 구현
    
    * feat - #20: Footer Component 구현
    
    * feat - #20: Login/Signup Modal 구현

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,11 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js, jsx, ts, tsx}",
-    "./components/**/*.{js, jsx, ts, tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainColor: "#1F4DED",
+        mainUpColor: "#0034EB",
+        mainDownColor: "#496CE7",
+        borderColor: "rgb(228, 228, 235)",
+      },
+      transitionProperty: {},
+      keyframes: {
+        slideFadeInDropdownAnimation: {
+          "0%": {
+            transform: "translateY(-100%)",
+          },
+          "100%": {
+            transform: "translateY(0)",
+          },
+        },
+        slideFadeOutDropdownAnimation: {
+          "0%": {
+            transform: "translateY(0)",
+          },
+          "100%": {
+            transform: "translateY(-100%)",
+          },
+        },
+      },
+      animation: {},
+    },
   },
   plugins: [],
 };
