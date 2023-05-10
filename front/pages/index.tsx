95ea3aade24c564f8cb65c409991574e0f6b7eb1
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-05-11 03:15:30 +0900

    feat - #20: next layout 적용

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,15 +1,10 @@
-import { Header } from "../components/blocks/header";
-import { Footer } from "../components/blocks/footer";
-
 export default function Home() {
   return (
     <div>
-      <Header />
       <div className="h-[500px]">안녕</div>
       <div className="h-[500px]">안녕</div>
       <div className="h-[500px]">안녕</div>
       <div className="h-[500px]">안녕</div>
-      <Footer />
     </div>
   );
 }
