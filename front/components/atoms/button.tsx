b3cf7dea6cb1f128bfd412633569983d9ee418d9
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-05-11 17:11:59 +0900

    feat - #20: Home Page UI 구현

--- front/components/atoms/button.tsx
+++ front/components/atoms/button.tsx
@@ -5,14 +5,32 @@ interface ButtonPropsType {
   hoverScale?: boolean;
   hoverBg?: boolean;
   name: string;
+  style?: string;
 }
 
 export const MainButton: React.FC<ButtonPropsType> = (props) => {
   return (
     <button
       className={`bg-mainColor text-white w-full py-2 rounded-lg transition-all ${
-        props.hoverScale && "hover:scale-[1.03]"
-      } ${props.hoverBg && "hover:bg-mainUpColor"}`}
+        props.style && props.style
+      } ${props.hoverScale && "hover:scale-[1.03]"} ${
+        props.hoverBg && "hover:bg-mainUpColor"
+      }`}
+      onClick={() => props.onClick()}
+    >
+      {props.name}
+    </button>
+  );
+};
+
+export const SubButton: React.FC<ButtonPropsType> = (props) => {
+  return (
+    <button
+      className={`bg-white border-[1px] border-mainColor text-mainColor w-full py-2 rounded-lg transition-all ${
+        props.style && props.style
+      } ${props.hoverScale && "hover:scale-[1.03]"} ${
+        props.hoverBg && "hover:bg-mainUpColor"
+      }`}
       onClick={() => props.onClick()}
     >
       {props.name}
