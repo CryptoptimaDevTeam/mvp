a472b67feb526f317b4f84bb9d58a69523bb4385
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-05-25 16:36:49 +0900

    feat - #21: UID 등록 UI 및 기능 구현

---
+++ front/components/atoms/alert.tsx
@@ -0,0 +1,50 @@
+import { useEffect, Dispatch, SetStateAction } from "react";
+import { BiBlock, BiHelpCircle } from "react-icons/bi";
+
+interface AlertStatusType {
+  top: string;
+  isOpen: boolean;
+  message: string;
+  type: string;
+}
+
+interface AlertPropsType {
+  alertStatus: AlertStatusType;
+  setAlertStatus: Dispatch<SetStateAction<AlertStatusType>>;
+  time?: number;
+}
+
+export function Alert({ alertStatus, setAlertStatus, time }: AlertPropsType) {
+  useEffect(() => {
+    setTimeout(() => {
+      if (alertStatus.isOpen) {
+        setAlertStatus({ ...alertStatus, isOpen: false });
+      }
+    }, time || 3000);
+  }, [alertStatus.isOpen]);
+
+  const alertBoxClassName = alertStatus.isOpen ? "flex" : "hidden";
+
+  return (
+    <div
+      className={`alert-container fixed mx-auto ${
+        alertStatus.top ? alertStatus.top : "top-5"
+      } justify-center left-1/2 -translate-x-1/2 z-20  ${alertBoxClassName}`}
+    >
+      <div className="alert-wrapper py-2 px-4 flex justify-center items-center gap-2 animate-dropDown bg-mainColor text-white border-[1px] border-bordercolor rounded shadow-lg">
+        <div className="alert-icon flex items-center pt-[2px]">
+          {alertStatus.type === "caution" ? (
+            <BiBlock />
+          ) : alertStatus.type === "caution" ? (
+            <BiHelpCircle />
+          ) : (
+            <></>
+          )}
+        </div>
+        <div className="alert-message pt-[2px] t-[16px] flex items-center">
+          <span>{alertStatus.message}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
