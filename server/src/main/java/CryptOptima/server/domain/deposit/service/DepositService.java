cf2fdce8206aefe061356f852969b2d6c3ec8dac
YerimSW <swyr1016@gmail.com>
2023-05-29 12:17:27 +0900

    rename - #8: DepositService 메서드명 수정

--- server/src/main/java/CryptOptima/server/domain/deposit/service/DepositService.java
+++ server/src/main/java/CryptOptima/server/domain/deposit/service/DepositService.java
@@ -6,13 +6,11 @@ import java.util.List;
 
 public interface DepositService {
     public DepositDto.MngDeposit createDeposit(DepositDto.Create depositDto);
-    public List<DepositDto.UserDeposit> getDepositsByUserId(Long userId, Long exchangeId, Long coinId,
-                                                            String startDate, String endDate);
+    public List<DepositDto.UserDeposit> getUserDeposits(
+            Long userId, Long exchangeId, Long coinId, String startDate, String endDate);
 
-    public List<DepositDto.MngDeposit> getDepositsByManger(Long userId, Long exchangeId, Long coinId,
-                                                           String startDate, String endDate);
-    // 두 메서드는 동일한 Repo 메서드를 공유할 수 있겠다. Deposit 객체를 뽑아와서 해당 DTO로 매핑하여
-    // 컨트롤러에 넘겨주면 되겠다.
+    public List<DepositDto.MngDeposit> getMngDeposits(
+            Long userId, Long exchangeId, Long coinId, String startDate, String endDate);
 
     // TODO 필터링 기능을 구현할 때 optional한 필터는 어떻게 처리하는지
     // Service 단에서 인자에 따라 Repository 메서드들 여러개 구현하여 매핑하는지
