3c3110bb613249e67eb586df1919633e1f931753
YerimSW <swyr1016@gmail.com>
2023-05-30 11:04:51 +0900

    feat - #4: OAuth2 구글 로그인 registrationId 필드 추가, OAuth2 사용자 정보 업데이트 기능 추가

--- server/src/main/java/CryptOptima/server/domain/user/UserDto.java
+++ server/src/main/java/CryptOptima/server/domain/user/UserDto.java
@@ -1,13 +1,13 @@
 package CryptOptima.server.domain.user;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 public class UserDto {
 
     @Getter
     @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     static class Create {
         private String accountId;
         private String status;
@@ -15,15 +15,23 @@ public class UserDto {
 
     @Getter
     @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     static class Update {
         private String status;
     }
 
+    @Getter
     @Setter
     @Builder
     static class Response {
         private long userId;
         private String accountId;
+        private String sns;
+        private String username;
         private String status;
+        private String paybackCumAmount;
+        private String paybackFinishedAmount;
+        private String paybackTotalRequestedAmount;
     }
 }
