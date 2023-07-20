5c0d2975f48901d41bc81c311db4257784a89ed2
YerimSW <swyr1016@gmail.com>
2023-07-21 00:06:00 +0900

    feat - #67: 비즈니스 로직, 시스템 예외처리 분리

--- server/src/main/java/CryptOptima/server/global/exception/ErrorResponse.java
+++ server/src/main/java/CryptOptima/server/global/exception/ErrorResponse.java
@@ -14,12 +14,13 @@ public class ErrorResponse {
         this.message = message;
     }
 
+    // 비즈니스로직 예외: 지정된 Code로 생성할 경우
     public static ErrorResponse of(ExceptionCode exceptionCode) {
         return new ErrorResponse(exceptionCode.getStatus(), exceptionCode.getMessage());
     }
 
-    public static ErrorResponse of(HttpStatus status) {
-        return new ErrorResponse(status.value(), status.getReasonPhrase());
+    // 런타임 예외: 서버 시스템 상 에러
+    public static ErrorResponse of(Exception e) {
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
     }
-    // TODO Controller DTO 유효성 검증 에러 핸들러 추가
 }
