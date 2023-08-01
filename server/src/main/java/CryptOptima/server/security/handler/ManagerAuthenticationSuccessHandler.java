18fb2ca7f1492d34396bd6a6584c92d25303a378
YerimSW <swyr1016@gmail.com>
2023-08-01 11:32:33 +0900

    fix - #67: AccessToken 재발급, 로그아웃 수정

---
+++ server/src/main/java/CryptOptima/server/security/handler/ManagerAuthenticationSuccessHandler.java
@@ -0,0 +1,38 @@
+package CryptOptima.server.security.handler;
+
+import CryptOptima.server.domain.manager.Manager;
+import CryptOptima.server.security.jwt.JwtService;
+import CryptOptima.server.security.jwt.JwtTokenizer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ManagerAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+
+        String accessToken = JwtTokenizer.generateAccessToken(authentication.getPrincipal());
+        String refreshToken = JwtTokenizer.generateRefreshToken(authentication.getPrincipal());
+
+        // redis에 RT 저장
+        jwtService.login(((Manager) authentication.getPrincipal()).getAccountId(), refreshToken);
+
+        response.setHeader("Authorization", "Bearer" + accessToken);
+        response.addHeader("Refresh", refreshToken);
+    }
+}
