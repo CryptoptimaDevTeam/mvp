ec4f80344383eed1155421b496d4dc944937b48b
YerimSW <swyr1016@gmail.com>
2023-08-24 19:33:28 +0900

    fix: validation 라이브러리 import문 삭제

--- server/src/main/java/CryptOptima/server/domain/manager/ManagerController.java
+++ server/src/main/java/CryptOptima/server/domain/manager/ManagerController.java
@@ -7,8 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/server/managers")
