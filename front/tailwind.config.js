2e219ecf80dc406871acb5097a4c49300a3e4f59
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-08-29 19:10:03 +0900

    Revert "feat: landing page front-end 작업 85% 완료 (#84)"
    This reverts commit 940ce0471b2197aacfa69b6b2037b0ee07e1b042.

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,50 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './layout/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layout/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        mainColor: '#1F4DED',
-        mainUpColor: '#0034EB',
-        mainDownColor: '#496CE7',
-        borderColor: 'rgb(228, 228, 235)',
-        textGrayColor: '#72717d',
-        redColor: '#D8494A',
+        mainColor: "#1F4DED",
+        mainUpColor: "#0034EB",
+        mainDownColor: "#496CE7",
+        borderColor: "rgb(228, 228, 235)",
+        textGrayColor: "#72717d",
+        redColor: "#D8494A",
       },
-      transitionProperty: {
-        height: 'height',
-        size: 'heigt, width',
-        border: 'border',
-      },
+      transitionProperty: {},
       keyframes: {
         slideFadeInDropdownAnimation: {
-          '0%': {
-            transform: 'translateY(-100%)',
+          "0%": {
+            transform: "translateY(-100%)",
           },
-          '100%': {
-            transform: 'translateY(0)',
+          "100%": {
+            transform: "translateY(0)",
           },
         },
         slideFadeOutDropdownAnimation: {
-          '0%': {
-            transform: 'translateY(0)',
+          "0%": {
+            transform: "translateY(0)",
           },
-          '100%': {
-            transform: 'translateY(-100%)',
+          "100%": {
+            transform: "translateY(-100%)",
           },
         },
         dropDown: {
-          '0%': { transform: 'translateY(-100%) ' },
-          '100%': { transform: 'translateY(0) ' },
+          "0%": { transform: "translateY(-100%) " },
+          "100%": { transform: "translateY(0) " },
         },
       },
       animation: {
-        dropDown: 'dropDown 0.5s ease',
+        dropDown: "dropDown 0.5s ease",
       },
     },
   },
