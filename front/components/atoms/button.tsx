b79fee815f1a11b4687219ae8a9e489a3ecf57c7
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-08-29 19:10:03 +0900

    Revert "Revert "feat: landing page front-end 작업 85% 완료 (#84)""
    This reverts commit 0662617130b1341c1a6fc953a02aec76db1002d6.

--- front/components/atoms/button.tsx
+++ front/components/atoms/button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 interface ButtonPropsType {
   onClick: () => void;
@@ -9,22 +9,22 @@ interface ButtonPropsType {
   disabled?: boolean;
   className?: string;
   width?: string;
-  type?: "button" | "submit" | "reset";
+  type?: 'button' | 'submit' | 'reset';
 }
 
 export const MainButton: React.FC<ButtonPropsType> = (props) => {
   return (
     <button
       className={`bg-mainColor text-white ${
-        props.width ? props.width : "w-full"
+        props.width ? props.width : 'w-full'
       } py-2 rounded-lg transition-all ${props.style && props.style} ${
-        props.className ? props.className : ""
-      } ${props.hoverScale && "hover:scale-[1.03]"} ${
-        props.hoverBg && "hover:bg-mainUpColor"
+        props.className ? props.className : ''
+      } ${props.hoverScale && 'hover:scale-[1.01]'} ${
+        props.hoverBg && 'hover:bg-mainUpColor'
       }`}
       onClick={() => props.onClick()}
       disabled={props.disabled}
-      type={props.type || "button"}
+      type={props.type || 'button'}
     >
       {props.name}
     </button>
@@ -35,15 +35,15 @@ export const SubButton: React.FC<ButtonPropsType> = (props) => {
   return (
     <button
       className={`bg-white border-[1px] border-mainColor text-mainColor ${
-        props.width ? props.width : "w-full"
+        props.width ? props.width : 'w-full'
       } py-2 rounded-lg transition-all ${props.style && props.style} ${
-        props.className ? props.className : ""
-      } ${props.hoverScale && "hover:scale-[1.03]"} ${
-        props.hoverBg && "hover:bg-mainUpColor"
+        props.className ? props.className : ''
+      } ${props.hoverScale && 'hover:scale-[1.01]'} ${
+        props.hoverBg && 'hover:bg-[#j5j5j5]'
       }`}
       onClick={() => props.onClick()}
       disabled={props.disabled}
-      type={props.type || "button"}
+      type={props.type || 'button'}
     >
       {props.name}
     </button>
@@ -54,13 +54,13 @@ export const NormalButton: React.FC<ButtonPropsType> = (props) => {
   return (
     <button
       className={`border-[1px] ${
-        props.width ? props.width : "w-full"
-      } py-2 transition-all ${props.style ? props.style : ""} ${
-        props.className ? props.className : ""
-      } ${props.hoverScale && "hover:scale-[1.03]"}`}
+        props.width ? props.width : 'w-full'
+      } py-2 transition-all ${props.style ? props.style : ''} ${
+        props.className ? props.className : ''
+      } ${props.hoverScale && 'hover:scale-[1.03]'}`}
       onClick={() => props.onClick()}
       disabled={props.disabled}
-      type={props.type || "button"}
+      type={props.type || 'button'}
     >
       {props.name}
     </button>
