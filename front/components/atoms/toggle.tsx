f03e5310f669d2a9c947201cc25d27bcad0175c2
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-08-29 19:10:03 +0900

    Revert "feat - #22: comparison table 구현 및 사용자 관련 기능 추가"
    This reverts commit 334fbc91425000702461481fce13d50725d0ef7d.

--- front/components/atoms/toggle.tsx
+++
@@ -1,8 +0,0 @@
-interface TogglePropsType {
-  isOn: boolean;
-  setIsOn: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-export const Toggle = ({ isOn, setIsOn }: TogglePropsType) => {
-  return <div></div>;
-};
--- front/components/blocks/header.tsx
+++ front/components/blocks/header.tsx
@@ -8,8 +8,7 @@ import { FaMoon, FaSun } from "react-icons/fa";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { headerNavList } from "../../data/headerStatic";
-import { useAppSelector, useAppDispatch } from "../../ducks/store";
-import { initLoginIdentity } from "../../ducks/loginIdentitySlice";
+import { useAppSelector } from "../../ducks/store";
 
 interface HeaderPropsType {}
 
@@ -18,7 +17,6 @@ type loginSignupVar = "none" | "login" | "signup";
 
 export const Header: React.FC<HeaderPropsType> = () => {
   const router = useRouter();
-  const dispatch = useAppDispatch();
   const { pathname } = router;
   const { isLogin, username } = useAppSelector((state) => state.loginIdentity);
 
@@ -248,12 +246,7 @@ export const Header: React.FC<HeaderPropsType> = () => {
             </ul>
           </div>
           {isLogin && (
-            <div
-              className="signout-btn py-5 text-[15px] border-borderColor border-t-[1px] cursor-pointer hover:text-mainDownColor"
-              onClick={() => {
-                dispatch(initLoginIdentity());
-              }}
-            >
+            <div className="signout-btn py-5 text-[15px] border-borderColor border-t-[1px] cursor-pointer hover:text-mainDownColor hidden">
               Sign Out
             </div>
           )}
