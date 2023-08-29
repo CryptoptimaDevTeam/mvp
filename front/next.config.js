04b1197f46d82eb129618f9fb8aa71781c3a1583
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-08-29 19:10:03 +0900

    Revert "Iss20 (#25)"
    This reverts commit 264a99d9558469e275e43341962ae93129ac41db.

--- front/next.config.js
+++ front/next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+  swcMinify: true,
+}
 
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
