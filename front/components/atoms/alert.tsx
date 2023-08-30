94ce0c906c577f91e7acd905ba07274ddc439dca
park jeong woo <pjwp94@gmail.com>
2023-08-30 20:53:33 +0900

    fix - #92: 환경변수 설정 관련 에러 수정 및 반응형 css 도입

--- front/components/atoms/alert.tsx
+++ front/components/atoms/alert.tsx
@@ -1,8 +1,8 @@
-import { useEffect, Dispatch, SetStateAction } from "react";
-import { BiBlock, BiHelpCircle } from "react-icons/bi";
+import { useEffect, Dispatch, SetStateAction } from 'react';
+import { BiBlock, BiHelpCircle } from 'react-icons/bi';
 
 interface AlertStatusType {
-  top: string;
+  top?: string;
   isOpen: boolean;
   message: string;
   type: string;
@@ -23,28 +23,28 @@ export function Alert({ alertStatus, setAlertStatus, time }: AlertPropsType) {
     }, time || 4000);
   }, [alertStatus.isOpen]);
 
-  const alertBoxClassName = alertStatus.isOpen ? "flex" : "hidden";
+  const alertBoxClassName = alertStatus.isOpen ? 'flex' : 'hidden';
 
   return (
     <div
       className={`alert-container fixed mx-auto ${
-        alertStatus.top ? alertStatus.top : "top-5"
+        alertStatus.top ? alertStatus.top : 'top-5'
       } justify-center left-1/2 -translate-x-1/2 z-20  ${alertBoxClassName}`}
     >
       <div
-        className="alert-wrapper py-2 px-4 flex justify-center items-center gap-2 animate-dropDown 
-      bg-mainColor text-white border-[1px] border-bordercolor rounded shadow-lg"
+        className='alert-wrapper py-2 px-4 flex justify-center items-center gap-2 animate-dropDown 
+      bg-mainColor text-white border-[1px] border-bordercolor rounded'
       >
-        <div className="alert-icon flex items-center pt-[2px]">
-          {alertStatus.type === "caution" ? (
+        <div className='alert-icon flex items-center pt-[2px]'>
+          {alertStatus.type === 'caution' ? (
             <BiBlock />
-          ) : alertStatus.type === "caution" ? (
+          ) : alertStatus.type === 'caution' ? (
             <BiHelpCircle />
           ) : (
             <></>
           )}
         </div>
-        <div className="alert-message pt-[2px] t-[16px] flex items-center">
+        <div className='alert-message pt-[2px] t-[16px] flex items-center'>
           <span>{alertStatus.message}</span>
         </div>
       </div>
