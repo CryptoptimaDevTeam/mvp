60651658fdbfed1a037a314ff7ad7c42b0f9f454
YerimSW <swyr1016@gmail.com>
2023-09-02 10:23:31 +0900

    feat - #105: 동적권한 관련 entity 추가

---
+++ server/src/main/java/CryptOptima/server/domain/role/entity/Grade.java
@@ -0,0 +1,31 @@
+package CryptOptima.server.domain.role.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Grade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long gradeId;
+
+    @Column(unique = true)
+    String gradeName;
+
+    @OneToMany(mappedBy = "grade", cascade = CascadeType.ALL)
+    List<GradeRole> gradeRoleList = new ArrayList<>();
+
+    public void updateGradeRoleList(List<GradeRole> gradeRoleList) {
+        this.gradeRoleList = gradeRoleList;
+    }
+}
