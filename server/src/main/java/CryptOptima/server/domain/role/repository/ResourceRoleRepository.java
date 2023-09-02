f4c76ffad4c7a53b33ded1010e8ebb172f1a6b6e
YerimSW <swyr1016@gmail.com>
2023-09-02 12:56:59 +0900

    feat - #105: Resource 수정,삭제 구현

---
+++ server/src/main/java/CryptOptima/server/domain/role/repository/ResourceRoleRepository.java
@@ -0,0 +1,7 @@
+package CryptOptima.server.domain.role.repository;
+
+import CryptOptima.server.domain.role.entity.ResourceRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResourceRoleRepository extends JpaRepository<ResourceRole, Long> {
+}
