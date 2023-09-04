aa8425fb53fd75e73f7dca49049ffef97230da23
YerimSW <swyr1016@gmail.com>
2023-09-04 17:40:36 +0900

    fix: public 접근 리소스 설정

--- server/src/main/java/CryptOptima/server/domain/role/controller/ResourceController.java
+++ server/src/main/java/CryptOptima/server/domain/role/controller/ResourceController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/server/admin/resources")
+@RequestMapping("/server/managers/resources")
 @RequiredArgsConstructor
 public class ResourceController {
 
--- server/src/main/java/CryptOptima/server/domain/survey/controller/SurveyController.java
+++ server/src/main/java/CryptOptima/server/domain/survey/controller/SurveyController.java
@@ -12,19 +12,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/server/survey")
+@RequestMapping("/server")
 @RequiredArgsConstructor
 public class SurveyController {
 
     private final SurveyService surveyService;
 
-    @PostMapping
+    @PostMapping("/public/survey")
     public ResponseEntity createSurvey(@RequestBody SurveyDto.Create surveyCreateDto) {
 
         return new ResponseEntity(surveyService.createSurvey(surveyCreateDto), HttpStatus.OK);
     }
 
-    @GetMapping("/groups") // TODO 필터링 조건을 RequestParam 으로 받을지, DTO로 받을지
+    @GetMapping("/managers/survey/groups") // TODO 필터링 조건을 RequestParam 으로 받을지, DTO로 받을지
     public ResponseEntity getSurveyGroups(@RequestBody SurveyDto.GroupCondition groupCond) {
         SurveyDto.GroupResponse response = SurveyDto.GroupResponse.builder()
                 .groupCondition(groupCond) // group by 그룹핑 조건
@@ -34,7 +34,7 @@ public class SurveyController {
         return new ResponseEntity(response, HttpStatus.OK);
     }
 
-    @GetMapping // TODO 필터링 조건을 RequestParam 으로 받을지, DTO로 받을지
+    @GetMapping("/managers/survey")
     public ResponseEntity getSurveysWhere(@RequestBody SurveyDto.SelectCondition selectCond) {
 
         SurveyDto.SelectResponse response = SurveyDto.SelectResponse.builder()
