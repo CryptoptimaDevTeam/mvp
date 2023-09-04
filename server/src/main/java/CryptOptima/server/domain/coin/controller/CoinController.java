2022687edff7aa5dff18c0a3a7b37ede5700b00a
YerimSW <swyr1016@gmail.com>
2023-09-04 17:40:36 +0900

    fix: public 접근 리소스 설정

--- server/src/main/java/CryptOptima/server/domain/coin/controller/CoinController.java
+++ server/src/main/java/CryptOptima/server/domain/coin/controller/CoinController.java
@@ -15,18 +15,18 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("/server")
 public class CoinController {
+    // TODO coin 시세 업데이트 관련 로직 수정.
 
     private final CoinService coinService;
 
     // MNG_UPDATE01 :: 주요 코인들의 시세를 업데이트 한다.
-    // TODO RequestBody type 정하기. { String: value }Map or String
-    @PatchMapping("/managers/crypto-price")
+    @PatchMapping("/managers/crypto")
     public ResponseEntity updateCoins(@RequestBody Map<String, String> coinValues) {
         return new ResponseEntity(HttpStatus.OK);
     }
 
     // MNG_UPDATE01 :: 주요 코인의 시세를 업데이트 한다.
-    @PatchMapping("/managers/crypto-price/{coin-id}")
+    @PatchMapping("/managers/crypto/{coin-id}")
     public ResponseEntity updateCoin(@PathVariable("coin-id") Long coinId,
                                      @RequestBody CoinDto.Update coinDto) {
         coinService.updateCoin(coinDto, coinId);
@@ -34,7 +34,7 @@ public class CoinController {
     }
 
     // MNG_UPDATE02 :: 주요 코인들의 시세를 조회한다.
-    @GetMapping("/public/crypto-price")
+    @GetMapping("/public/crypto")
     public ResponseEntity getCoins(@RequestParam(value = "page", defaultValue = "1") int page,
                                    @RequestParam(value = "size", defaultValue = "20") int size) {
         List<CoinDto.Response> response = coinService.getCoins(page-1, size);
@@ -42,7 +42,7 @@ public class CoinController {
     }
 
     // MNG_UPDATE03 :: 신규 코인을 등록한다.
-    @PostMapping("/managers/crypto-price")
+    @PostMapping("/managers/crypto")
     public ResponseEntity createCoin(@RequestBody CoinDto.Create coinDto) {
         coinService.createCoin(coinDto);
         return new ResponseEntity(HttpStatus.CREATED);
