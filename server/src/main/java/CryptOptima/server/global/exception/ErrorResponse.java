8aefc38ab975a1692599466fc8454c2482231e42
YerimSW <swyr1016@gmail.com>
2023-09-06 08:57:49 +0900

    feat - #108: DTO 유효성 검증 예외처리

--- server/src/main/java/CryptOptima/server/global/exception/ErrorResponse.java
+++ server/src/main/java/CryptOptima/server/global/exception/ErrorResponse.java
@@ -1,26 +1,63 @@
 package CryptOptima.server.global.exception;
 
-import lombok.Getter;
+import lombok.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class ErrorResponse {
 
-    private int status;
-    private String message;
-
-    private ErrorResponse(int status, String message) {
-        this.status = status;
-        this.message = message;
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Default {
+        private int status;
+        private String message;
     }
 
     // 비즈니스로직 예외: 지정된 Code로 생성할 경우
-    public static ErrorResponse of(ExceptionCode exceptionCode) {
-        return new ErrorResponse(exceptionCode.getStatus(), exceptionCode.getMessage());
+    public static ErrorResponse.Default of(ExceptionCode exceptionCode) {
+        return new ErrorResponse.Default(exceptionCode.getStatus(), exceptionCode.getMessage());
     }
 
     // 런타임 예외: 서버 시스템 상 에러
-    public static ErrorResponse of(Exception e) {
-        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+    public static ErrorResponse.Default of(Exception e) {
+        return new ErrorResponse.Default(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Validation {
+        private int status;
+        private String message;
+        private List<FieldError> fieldErrors;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class FieldError {
+        private String field;
+        private String error;
+    }
+
+    public static ErrorResponse.Validation of(MethodArgumentNotValidException e) {
+        return Validation.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("The value you entered is not valid.")
+                .fieldErrors(
+                        e.getFieldErrors().stream().map(
+                                fieldError -> FieldError.builder()
+                                        .field(fieldError.getField())
+                                        .error(fieldError.getDefaultMessage())
+                                        .build()
+                        ).collect(Collectors.toList())
+                ).build();
     }
 }
